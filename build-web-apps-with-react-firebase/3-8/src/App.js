3a15111807619b1e8f612f2652de532bd03e074e
Michael Taggart <mtaggarthawk@gmail.com>
2022-01-20 13:46:14 -0700

    7 using state to keep track of input values

--- build-web-apps-with-react-firebase/3-8/src/App.js
+++ build-web-apps-with-react-firebase/3-8/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import EventList from './components/EventList';
 import Modal from './components/Modal';
 import Title from "./components/Title";
+import NewEventForm from './components/NewEventForm';
 
 function App() {
   const [showModal, setShowModal] = useState(true);
@@ -13,6 +14,13 @@ function App() {
     { title: 'race on moo moo farm', id: 3 },
   ])
 
+  const handleEvents = (newEvent) => {
+    setEvents([...events, newEvent])
+    // setEvents((prevEvents) => {
+    //   return [...prevEvents, newEvent]
+    // })
+  }
+
   const handleDeleteEvent = (id) => {
     setEvents(
       events.filter((event) => {
@@ -20,7 +28,7 @@ function App() {
       })
     )
   }
-  const handleClose = () =>{
+  const handleClose = () => {
     setShowModal(false);
   }
 
@@ -39,17 +47,16 @@ function App() {
         <button onClick={() => setShowEvents(true)}>Show Events</button>
       </div>}
 
-      {showEvents && <EventList events={events} handleDeleteEvent={handleDeleteEvent}/>}
+      {showEvents && <EventList events={events} handleDeleteEvent={handleDeleteEvent} />}
 
       <br />
       <button onClick={() => setShowModal(true)}>Show Modal</button>
 
-{
-  showModal && <Modal handleClose={handleClose} isSalesModal={true}>
-        <h1>10% Off Coupon Code!</h1>
-        <p>Use the code NINJA10 at the checkout</p>
-      </Modal>
-}
+      {
+        showModal && <Modal handleClose={handleClose} isSalesModal={true}>
+          <NewEventForm handleClose={handleClose} handleEvents={handleEvents} />
+        </Modal>
+      }
     </div>
   );
 }
--- build-web-apps-with-react-firebase/3-8/src/components/newEventForm.js
+++ build-web-apps-with-react-firebase/3-8/src/components/newEventForm.js
@@ -1,21 +1,58 @@
-import "./newEventForm.css";
+import { useState } from "react";
+import "./NewEventForm.css";
 
-export default function newEventForm() {
+export default function NewEventForm({handleEvents, handleClose}) {
+
+ const [title, setTitle] = useState("");
+ const [date, setDate] = useState("");
+
+ // const handleChange = (event) =>{
+ //  setTitle(event.target.value);
+ // }
+
+ const resetForm = (event) => {
+  setTitle("");
+  setDate("");
+ }
+
+ const handleSubmit = (e) => {
+  e.preventDefault();
+
+  const event = {
+   title: title,
+   date: date,
+   id: Math.floor(Math.random() * 10000),
+  }
+  handleEvents(event);
+  handleClose()
+  resetForm();
+ }
 
  return (
-  <form className="new-event-form">
+  <form className="new-event-form" onSubmit={handleSubmit}>
    <label>
     <span>Event Title:</span>
-    <input type="text" placeholder="Event Title" />
+    <input
+     type="text"
+     placeholder="Event Title"
+     onChange={(e) => setTitle(e.target.value)}
+     value={title}
+    />
    </label>
 
    <label>
     <span>Event Date:</span>
-    <input type="date" placeholder="Event Date" />
+    <input
+     type="date"
+     placeholder="Event Date"
+     onChange={(e) => setDate(e.target.value)}
+     value={date}
+    />
    </label>
 
    <button>Submit Form</button>
-
+   <p> title - {title}, date - {date}</p>
+   <p onClick={resetForm}>reset the form</p>
   </form>
  );
 }
