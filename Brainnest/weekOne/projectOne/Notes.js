153b4ba2c51b7a101b519ab5c39563b04df4c64e
Michael Taggart <mtaggarthawk@gmail.com>
2022-06-27 22:14:37 -0600

    feat: questions answered

---
+++ Brainnest/weekOne/projectOne/Notes.js
@@ -0,0 +1,130 @@
+/*
+? 1. What do HTML and CSS stand for?
+HTML = Hypertext Markup Language
+
+CSS = Cascading Style Sheets
+
+? 2. Would you rather use HTML or CSS, for putting paragraphs of text on a webpage?
+HTML. Paragraphs of text is web page structure and not styling
+
+? 3. Would you rather use HTML or CSS, to change the font and background color of a
+button?
+CSS. colors and fonts are styling and not structure.
+
+? 4. What is an HTML tag?
+A tag is a special syntax that determines the way a browser will interpret a section of the webpage
+
+? 5. What are the three parts of an HTML element? (image from paragraph)
+The opening tag. The content (inside). The closing tag.
+
+? 6. What is the purpose of the doctype declaration?
+This tells the browser how it should interpret the rest of the document.
+
+? 7. What is the HTML element?
+A way to describe a certain part of a web page.
+
+? 8. What is the purpose of the head element?
+To describe additional settings for the rest of the html
+
+? 9. What is the purpose of the body element?
+To display the content of the webpage
+
+? 10. How do you create a paragraph in HTML?
+<p></p> => one can use the 'p' tag in this manner
+
+? 11. How do you create a heading in HTML?
+<h1> - <h6> => using these 'h#' tag
+
+? 12. How many different levels of headings are there and what is the difference between them?
+There are 6 different levels. They share the level of importance of the text from most (<h1>) to least (<h6>).
+
+? 13. What element should you use to make text bold and important?
+<strong></strong> => This makes text important and bold
+<b></b> => This makes text bold without any importance
+
+? 14. What element should you use to make text italicized to add emphasis to it?
+<em></em> => This makes text italicized and adds emphasis on it
+<i></i> => This makes text italicized without any emphasis
+
+? 15. What relationship does an element have with any nested elements within it?
+It is the parent element of the nested elements within it (children).
+
+? 16. What relationship do two elements have if they are at the same level of nesting?
+Siblings
+
+? 17. How do you create HTML comments?
+<!-- -->  => This is how you make comments in HTML
+
+? 18. What HTML tag is used to create an unordered list?
+<ul></ul>
+
+? 19. What HTML tag is used to create an ordered list?
+<ol></ol>
+
+? 20. What HTML tag is used to create list items within both unordered and ordered lists?
+<li></li>
+
+? 21. What element is used to create a link?
+<a></a>
+
+? 22. What is an attribute?
+A description that adds additional information to a tag
+
+? 23. What attribute tells links where to go?
+href
+
+? 24. What is the difference between an absolute and relative link?
+An absolute link has the complete web address for a particular webpage
+A relative link has an incomplete web address. Usually referencing another page on the current website.
+
+? 25. Which element is used to display an image?
+<img />
+
+? 26. What two attributes do images always need to have?
+src => where the image is stored
+alt => text that describes the image
+
+? 27. How do you access a parent directory in a file path?
+using a single dot '.' references the current directory. Use two dots '../' to go up to the parent directory
+
+? 28. What are the four main image formats that you can use for images on the web?
+JPEG/JPG => Great for static images, can lose quality of image (NO: text, animation, transparency, etc.)
+
+PNG => They do not lose quality of image, can support: animation, transparency
+
+GIF => They do not lose quality of image, but smaller than JPEG (only 256 colors)
+
+SVG => Retain full resolution when scaled at any size. (Large file)
+*/
+
+/*
+* How to ask for help
+
+? "How to be great at asking coding questions" => https://medium.com/@gordon_zhu/how-to-be-great-at-asking-questions-e37be04d0603
+
+? "The XY Problem" => https://xyproblem.info/
+
+* Useful links
+
+? “Why Learning to Code is So Damn Hard” => https://www.thinkful.com/blog/why-learning-to-code-is-so-damn-hard/
+
+? "Things I Wish Someone Had Told Me When I Was Learning How To Code" => https://www.freecodecamp.org/news/things-i-wish-someone-had-told-me-when-i-was-learning-how-to-code-565fc9dcb329/
+
+? "Deliberate Programming Practice" => https://codequizzes.wordpress.com/2013/04/28/deliberate-programming-practice/
+
+* HTML
+
+? The basics differences HTMLvsCSSvsJS => https://brytdesigns.com/html-css-javascript-whats-the-difference/
+
+? HTML & CSS for beginners => https://www.youtube.com/watch?v=LGQuIIv2RVA&list=PL4-IK0AVhVjM0xE0K2uZRvsM7LkIhsPT-&ab_channel=KevinPowell
+
+? BASIC HTML & CSS for NON-WEB DESIGNERS => http://www.dontfeartheinternet.com/02-html/
+
+? Generate random text => https://loremipsum.io/
+
+? Links & Images => https://www.internetingishard.com/html-and-css/links-and-images/
+
+? listed html tags => https://developer.mozilla.org/en-US/docs/Web/HTML/Element
+
+? Semantic elements => https://www.w3schools.com/html/html5_semantic_elements.asp
+*/
